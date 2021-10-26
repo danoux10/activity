a6f3adb09e92f555562936dd4fae02212007016d
danoux10 <danybarbe10@gmail.com>
2021-10-26 23:44:19 +0200

    navbar responsive & switch mode

---
+++ js/sript.js
@@ -0,0 +1,17 @@
+const btnToggle = document.querySelector('.switch');
+
+btnToggle.addEventListener('click', () => {
+
+    const body = document.body;
+
+    if(body.classList.contains('green')){
+
+        body.classList.add('red')
+        body.classList.remove('green')
+    } else if(body.classList.contains('red')){
+
+        body.classList.add('green')
+        body.classList.remove('red')
+    }
+
+})
