5aee18689fd6d9452d03f13a4347d2eb28b6eb4b
danoux10 <danybarbe10@gmail.com>
2021-10-31 13:49:50 +0100

    style graph form large

--- js/sript.js
+++ js/sript.js
@@ -1,3 +1,4 @@
+//swtich
 const btnToggle = document.querySelector('.switch');
 
 btnToggle.addEventListener('click', () => {
@@ -14,4 +15,44 @@ btnToggle.addEventListener('click', () => {
         body.classList.remove('red')
     }
 
-})
+});
+
+//btn form change
+var ajout = document.getElementById('ajout');
+var modif = document.getElementById('modif');
+var ajouter = document.getElementById('ajouter');
+var modifier = document.getElementById('modifier');
+
+ajout.classList.add('active');
+modif.classList.add('desactive');
+ajouter.classList.add('visible');
+modifier.classList.add('hidden');
+
+function viewAdd(){
+    if(ajout.classList.contains('desactive')){
+        modif.classList.add('desactive');
+        ajouter.classList.add('visible');
+        ajout.classList.add('active');
+        modifier.classList.add('hidden');
+        modif.classList.remove('active');
+        ajouter.classList.remove('hidden');
+        ajout.classList.remove('desactive');
+        modifier.classList.remove('visible');
+    }
+}
+
+function viewModif(){
+    if(modif.classList.contains('desactive')){
+        modif.classList.add('active');
+        ajouter.classList.add('hidden');
+        ajout.classList.add('desactive');
+        modifier.classList.add('visible');
+        modif.classList.remove('desactive');
+        ajouter.classList.remove('visible');
+        ajout.classList.remove('active');
+        modifier.classList.remove('hidden');
+    }
+}
+
+ajout.addEventListener('click',viewAdd);
+modif.addEventListener('click',viewModif);
