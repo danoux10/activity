1fe4ec73b77d73b7cabdb33a944dcf8656ce836c
danoux10 <danybarbe10@gmail.com>
2021-11-24 09:33:58 +0100

    activity select

---
+++ include/selectActivity.php
@@ -0,0 +1,9 @@
+<?php
+ $selectActivity = $bdd->query('select * from cards inner join color on cards.style=color.idColor');
+foreach ($selectActivity as $dataSelect){
+	$id = $dataSelect['id'];
+	$fill = $dataSelect['fill'];
+	$color = $dataSelect['text'];
+	$name = $dataSelect['nameCard'];
+}
+ 
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+	header('location:model/graph.php');
+	die();
+?>
